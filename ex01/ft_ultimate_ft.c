04399967a2d335200047212dba02da3a77584f2c
Yuri Vieira dos Santos <yurivieiradossantos@Yuris-Laptop.local>
2024-08-18 14:46:27 -0300

    Exercicios do 0 ao 2

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,6 @@
+void ft_ultimate_ft(int *********nbr);
+
+void ft_ultimate_ft(int *********nbr){
+    *********nbr = 42;
+    
+}
---
+++ ex02/ft_swap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void ft_swap(int *a, int *b);
+
+void ft_swap(int *a, int *b){
+
+    *a = *b;
+    *b = *a;
+
+
+}
+
